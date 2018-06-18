eb2e8f7864b574dc764cd9feace4fe079b893d6c
sofia4 <sofia4@cumanuallyo.com>
2018-06-18 15:50:14 -0400

    1529351414

--- locustfile.py
+++ locustfile.py
@@ -7,11 +7,18 @@ for i in range(100):
     time.sleep(0.1)
     sys.stdout.write("\r" + animation[i % len(animation)])
     sys.stdout.flush()
-print ("End!")
 
-stop_at = 0
+stop_at = 100
 while True:
     num = random.randint(1, 100)
     if num == stop_at:
         break
     sys.stdout.write(num)
+
+while True:
+    animation = "|/-\\"
+    for i in range(100):
+        time.sleep(0.1)
+        sys.stdout.write("\r" + animation[i % len(animation)])
+        sys.stdout.flush()
+    
