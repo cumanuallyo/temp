0db4474e19458812641ec789197dbecae453f027
sofia4 <sofia4@cumanuallyo.com>
2018-06-18 15:44:33 -0400

    1529351073

--- locustfile.py
+++ locustfile.py
@@ -1,7 +1,17 @@
 import random
+import sys
+import time
+
+animation = "|/-\\"
+for i in range(100):
+    time.sleep(0.1)
+    sys.stdout.write("\r" + animation[i % len(animation)])
+    sys.stdout.flush()
+print "End!"
+
 stop_at = 0
 while True:
-    num = random.randint(1, 1000000)
+    num = random.randint(1, 100)
     if num == stop_at:
         break
-    print (num)
+    sys.stdout.write(num)
