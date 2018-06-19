b9c3ff345cbc84fbcdf1bae13429830da2e6cd28
sofia4 <sofia4@cumanuallyo.com>
2018-06-19 15:13:57 -0400

    1529435637

--- locustfile.py
+++ locustfile.py
@@ -2,23 +2,9 @@ import random
 import sys
 import time
 
-animation = "|/-\\"
-for i in range(100):
-    time.sleep(0.1)
-    sys.stdout.write("\r" + animation[i % len(animation)])
-    sys.stdout.flush()
 
-stop_at = 100
-while True:
+stop_at = 0
+for x in range(7):
     num = random.randint(1, 100)
-    if num == stop_at:
-        break
-    sys.stdout.write(str(num))
-
-while True:
-    animation = "|/-\\"
-    for i in range(100):
-        time.sleep(0.1)
-        sys.stdout.write("\r" + animation[i % len(animation)])
-        sys.stdout.flush()
-    
+    time.sleep( 300 )
+    print(num)
